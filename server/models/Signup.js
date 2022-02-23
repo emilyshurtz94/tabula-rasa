cd954db1aebad5693ba99370c4c01db5943d6d1a
Richard Rogers <treyrogers073@gmail.com>
2022-02-23 14:13:07 -0700

    completed signUpSchema in the Signup models

--- server/models/Signup.js
+++ server/models/Signup.js
@@ -2,6 +2,22 @@ const { Schema, model } = require('mongoose');
 
 const signUpSchema = new Schema({
 
+    username: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
 })
+
+const Signup = model('Signup', signUpSchema);
 
 module.exports = Signup;
