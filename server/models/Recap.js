44de9f10240d74a47979f9639cfef47a4fcd39a6
Richard Rogers <treyrogers073@gmail.com>
2022-02-23 14:07:49 -0700

    set up models files

---
+++ server/models/Recap.js
@@ -0,0 +1,7 @@
+const { Schema, model } = require('mongoose');
+
+const recapSchema = new Schema({
+
+})
+
+module.exports = Recap;
---
+++ server/models/Signup.js
@@ -0,0 +1,7 @@
+const { Schema, model } = require('mongoose');
+
+const signUpSchema = new Schema({
+
+})
+
+module.exports = Signup;
