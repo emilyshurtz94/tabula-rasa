a23c861160f307cb07673272fe42893ead5d8d34
Karrie Garzelloni <kgarzelloni@yahoo.com>
2022-02-23 14:39:21 -0700

    added quote seed file

---
+++ server/seeds/seed.js
@@ -0,0 +1,8 @@
+//TODO will need complete seed.js for quotes to seed the database and randomly select one for each day for the home page
+
+const db = require('../config/connection');
+const {  } = require('../models');
+
+const quotes = require('./quotes.json');
+
+console.log('Quotes seeded!');
